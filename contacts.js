714f4aa7b18a9a15238c59d212d77e92ab2deeb4
Serhii_Dykyi <vankyverd@gmail.com>
2020-09-06 16:54:02 +0300

    add remove list, getContacts

---
+++ contacts.js
@@ -0,0 +1,40 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.resolve(__dirname, "db", "contacts.json");
+
+async function listContacts() {
+  const contacts = await fs.readFile(contactsPath, {
+    encoding: "utf-8",
+  });
+  return JSON.parse(contacts);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contactById = contacts.find((contact) => contact.id === contactId);
+  return contactById;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.filter((contact) => contact.id !== contactId);
+  const resultAsJSON = JSON.stringify(result);
+  fs.writeFile(contactsPath, resultAsJSON);
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const id = contacts.length ? [...contacts].pop().id + 1 : 1;
+  const newContact = {
+    id,
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  const contactAsJSON = JSON.stringify(contacts);
+  fs.writeFile(contactsPath, contactAsJSON);
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
