38e8e571f85745b0f964942641fd4a0d32cca1b7
Serhii Dykyi <dykyy.s@gmail.com>
2020-09-15 20:33:27 +0300

    fix patch/updateContact

--- index.js
+++ index.js
@@ -1,73 +1,38 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-
 const express = require('express');
-
 const app = express();
+const contactsRouter = require('./router');
+const cors = require('cors');
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', '*');
+//   res.setHeader('Access-Control-Allow-Methods', '*');
+//   next();
+// });
+
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000' }));
+// app.use(addAllowOriginHeader);
+// app.options('*', addCorsHeaders);
+app.use('/contacts', contactsRouter);
+
+function addAllowOriginHeader(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  next();
+}
+function addCorsHeaders(req, res, next) {
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    req.headers['Access-control-request-method'],
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    req.headers['Access-control-request-headers'],
+  );
+  res.status(200).send();
+}
 
 app.listen(PORT, () => console.log(`Server works on port ${PORT}`));
-
-// const http = require("http");
-// const argv = require("yargs").argv;
-// const {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   addContact,
-// } = require("./contacts");
-// const { response } = require("express");
-
-// const parsedBody = (request) => {
-//   return new Promise((reselve, reject) => {
-//     let body = [];
-//     request
-//       .on("data", (chank) => {
-//         body.push(chank);
-//       })
-//       .on("end", () => {
-//         const parsedBody = JSON.parse(body.toString());
-//         reselve(parsedBody);
-//       })
-//       .on("err", (err) => {
-//         reject(err);
-//       });
-//   });
-// };
-
-// const server = http.createServer(async (request, responce) => {
-//   const { url, method } = request;
-//   switch (url) {
-//     case "/contacts":
-//       switch (method) {
-//         case "GET":
-//           const contacts = await listContacts();
-//           const parssedContacts = JSON.stringify(contacts);
-//           responce.setHeader("Content-Type", "application/json");
-//           responce.write(parssedContacts);
-//           responce.end();
-//           break;
-//         case "POST":
-//           const body = await parsedBody(request);
-//           console.log(body);
-//           await addContact(body);
-//           responce.statusCode = 201;
-//           responce.end();
-//           break;
-//         case "DELETE":
-//           const { query } = request;
-//           console.log(query);
-//           responce.end();
-//           break;
-//       }
-//       break;
-//     default:
-//       responce.write("<h1>not found</h1>");
-//       responce.end();
-//   }
-// });
-
-// server.listen(3000, (error) => {
-//   if (error) return console.error(error);
-//   console.log(`Server works on port ${3000}`);
-// });
