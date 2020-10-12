51cf95206a44171d54d512a1335d603bf5a60b2f
Serhii_Dykyi <vankyverd@gmail.com>
2020-10-13 00:25:50 +0300

    s

--- services/avatar.services.js
+++ services/avatar.services.js
@@ -2,14 +2,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const path = require('path');
 const AvatarGenerator =require('avatar-generator')
-
 const createAvatarUrl = id =>
   `${process.env.PUBLIC_FOLDER}/${process.env.IMAGE_FOLDER}/${id}.png`;
-
 async function madeAvatar(id) {
   const avatar = new AvatarGenerator({
     parts: ['background', 'face', 'clothes', 'head', 'hair', 'eye', 'mouth'],
-
     partsLocation: path.join(__dirname, `../${process.env.IMG_FOR_AVATAR}`),
     imageExtension: '.png',
   });
@@ -17,5 +14,4 @@ async function madeAvatar(id) {
   const image = await avatar.generate(`${id}`, variant);
   image.png().toFile(createAvatarUrl(id));
 }
-
 module.exports = { madeAvatar, createAvatarUrl };
